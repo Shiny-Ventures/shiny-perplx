c2863e635cc62bca4ccbecb499e43a07ef275e71
Tom Osman <tomcharlesosman@gmail.com>
2025-01-08 23:37:45 +0100

    Added Metadata

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,22 +2,22 @@ import type { MetadataRoute } from 'next'
  
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "ShinyPerplx - AI-powered Search Engine",
-    short_name: "ShinyPerplx",
+    name: "Shiny - Step into the future",
+    short_name: "Shiny",
     description: "A minimalistic AI-powered search engine that helps you find information on the internet using advanced AI models like GPT-4, Claude, and Grok",
     start_url: "/",
     display: "standalone",
     categories: ["search", "ai", "productivity"],
     icons: [
       {
-        src: "/logo.png",
+        src: "/icon.png",
         sizes: "any",
         type: "image/png"
       },
     ],
     screenshots: [
       {
-        src: "/opengraph-image.png",
+        src: "/open-graph-image.png",
         type: "image/png",
         sizes: "1200x630",
       }
