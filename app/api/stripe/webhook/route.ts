f74c35fcfbe2dc46e79781977cf6f6187c4116d0
Tom Osman <tomcharlesosman@gmail.com>
2025-01-10 22:18:08 +0100

    Change Stripe API Version

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -5,7 +5,7 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2023-10-16',
   typescript: true,
 })
 
